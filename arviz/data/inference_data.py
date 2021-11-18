e440a91a0e06d379c34974ae7e2b33420dc7b749
mmajde <marek.majde@gmail.com>
2021-11-18 15:31:00 +0100

    Update inference_data.py

--- arviz/data/inference_data.py
+++ arviz/data/inference_data.py
@@ -80,7 +80,7 @@ SUPPORTED_GROUPS_ALL = SUPPORTED_GROUPS + SUPPORTED_GROUPS_WARMUP
 InferenceDataT = TypeVar("InferenceDataT", bound="InferenceData")
 
 
-class InferenceData(Mapping[str, xr.Dataset]):
+class InferenceDataClass(Mapping[str, xr.Dataset]):
     """Container for inference data storage using xarray.
 
     For a detailed introduction to ``InferenceData`` objects and their usage, see
